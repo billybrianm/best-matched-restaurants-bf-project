e83f3a1fe4f00a19017e283c8b92dc060f15a50f
Billy Brian <billybrianm@hotmail.com>
2023-03-09 12:51:27 -0300

    first commit

---
+++ src/main/java/com/example/bestmatchedrestaurantsbf/BestMatchedRestaurantsBfApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestmatchedrestaurantsbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestMatchedRestaurantsBfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMatchedRestaurantsBfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bestmatchedrestaurantsbf/BestMatchedRestaurantsBfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestmatchedrestaurantsbf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMatchedRestaurantsBfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
