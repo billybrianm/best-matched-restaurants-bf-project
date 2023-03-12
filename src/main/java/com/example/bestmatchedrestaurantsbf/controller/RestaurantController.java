c503a0a78b01aab059dd9554755eb22f4cceb998
Billy Brian <billybrianm@hotmail.com>
2023-03-12 17:33:07 -0300

    allow cross origin requests

--- src/main/java/com/example/bestmatchedrestaurantsbf/controller/RestaurantController.java
+++ src/main/java/com/example/bestmatchedrestaurantsbf/controller/RestaurantController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("")
+@CrossOrigin(origins = "*", allowedHeaders = "*") // TODO: use spring security to deal with CORS
 @Slf4j
 public class RestaurantController {
     @Autowired
